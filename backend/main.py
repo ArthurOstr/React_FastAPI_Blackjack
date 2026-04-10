e2e1e93aa51a46b2944fe5290c41b0b76ff27980
ArthurOstr <automata4231@gmail.com>
2026-04-10 15:22:39 +0300

    Fix: configurating middleware to allow only frontend calls

--- backend/main.py
+++ backend/main.py
@@ -21,13 +21,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="Async Blackjack API", lifespan=lifespan)
-frontend_url = os.getenv("FRONTEND_URL", )
+frontend_url = os.getenv("FRONTEND_URL","http://localhost:3000")
 
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[frontend_url],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
